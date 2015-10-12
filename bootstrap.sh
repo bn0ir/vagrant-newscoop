27723f7a3f43ee8239325ee6124184b3bf98b182
blacknoir <blacknoir@blacknoir>
2015-10-12 16:02:43 +0500

    Fix sed {{alias}} replace in apache2 config

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,7 @@ apt-get update && apt-get upgrade -y
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password vagrant'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password vagrant'
 
-apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev apache2 git nodejs g++ libjpeg-dev libpng-dev libapache2-mod-php5 php5-cli php5-curl php5-mysql php5-gd php5-intl php5-xcache imagemagick mysql-server unzip
-apt-get install -y libapache2-mpm-itk  apache2-mpm-itk
+apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev apache2-mpm-itk git nodejs g++ libjpeg-dev libpng-dev libapache2-mod-php5 php5-cli php5-curl php5-mysql php5-gd php5-intl php5-xcache imagemagick mysql-server unzip
 
 #install tty.js
 npm install -g tty.js
@@ -20,9 +19,9 @@ mkdir /vagrant/app
 
 #set apache2 configs
 export SITEDOMAIN=`cat /vagrant/config/site-domain`
-sed -e s/\{\{alias\}\}/$SITEDOMAIN/g /vagrant/config/apache2.conf > /etc/apache2/sites-available/php.conf
+sed -e 's/\{\{alias\}\}/$SITEDOMAIN/g' /vagrant/config/apache2.conf > /etc/apache2/sites-available/php.conf
 ln -s /etc/apache2/sites-available/php.conf /etc/apache2/sites-enabled/php.conf
-rm /etc/apache2/sites-enabled/000-default.conf
+rm /etc/apache2/sites-enabled/default
 
 #add apache2 modules
 ln -s /etc/apache2/mods-available/rewrite.load /etc/apache2/mods-enabled/rewrite.load
